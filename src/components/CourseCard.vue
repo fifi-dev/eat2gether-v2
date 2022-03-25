279451c3eb813460116026a91f8eac4696a25e3d
“Fideline <fideline.bazana_ntomo@edu.devinci.fr>
2022-03-25 15:25:22 +0100

    display all courses

--- src/components/CourseCard.vue
+++ src/components/CourseCard.vue
@@ -1,6 +1,6 @@
 <template>
   <!-- Card -->
-  <card class="bg-gray-800 rounded-lg w-72 ml-12">
+  <card class="bg-gray-800 rounded-lg w-72 mx-6">
     <!-- Header -->
     <div class="grid grid-cols-3 bg-white text-black items-center px-5 py-3">
       <!-- Header Title -->
